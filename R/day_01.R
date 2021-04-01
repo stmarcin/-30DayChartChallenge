5c352a9c13d7ab80e0d2a8a69c03f10e21485354
Marcin <stmarcin@gazeta.pl>
2021-04-02 00:14:18 +0200

    clean code

--- R/day_01.R
+++ R/day_01.R
@@ -1,40 +1,38 @@
-# remotes::install_github("hrbrmstr/waffle")
+# Libraries
 library(tidyverse)
 library(waffle)
-library(trimisViz)
 library(ggtext)
 library(stringr)
 library(extrafont)
 loadfonts()
 
-nfig <- "fig_10" 
-
-
+# colors
 color_media <- c(
     "car" = "#a36a69",
     "transit" = "#3f97d6", 
     "walking" = "#6f927c",
     "bike" =  "#9ab3a3")
 
+# data
 df <- tibble(name = c("car", "transit", "walking", "bike"),
              label = c("car", "transit", "walking", "bike"),
              value = c(32, 47, 18, 3)) %>% 
     mutate(x = c(4, 11, 18, 20)) %>% 
     mutate(x.proc = c(4, 11, 18, 20.1)) 
 
+# plot
 df %>% 
     ggplot(aes(fill = name, values = value)) +
     geom_waffle(n_rows = 5, size = 0.33, colour = "white") +
     geom_text(aes(label = label, y = 6, x = x),
               family = "Ebrima",
-              size = 10) +
+              size = 9) +
     geom_text(aes(label = paste0(value, "%"), y = 3, x = x.proc),
               family = "Ebrima",
               size = 10, color = "white", fontface = "bold") +
     labs(
         title = "How Warsaw residents move"
     ) +
-    # coord_cartesian() +
     scale_fill_manual(values = color_media) +
     coord_equal(clip = 'off') +
     theme_void() +
@@ -56,7 +54,5 @@ df %>%
             color = "#fef7ee")
     )
 
-# ggsave(file = here::here("img", "fig_10.png"), dpi = 600,
-#        width = 16, height = 5)
-
+# export to png
 ggsave(here::here("img", "day_1.png"), height = 5, width = 16, dpi = 600)
